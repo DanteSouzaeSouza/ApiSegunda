5d891ea542fce295249deaf8949f5bd670339c0c
Dante Souza e Souza <lokinmodar@gmail.com>
2020-06-15 20:11:47 -0300

    Initial Commit

---
+++ ApiSegunda/Controllers/FuncionarioController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiSegunda.Controllers
+{
+    public class FuncionarioController : ApiController
+    {
+        // GET api/funcionario
+        public System.Collections.IEnumerable Get()
+        {
+            return new string[] {"value 1", "value 2"};
+        }
+        // GET api/funcionario/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+        // POST api/funcionario
+        public void Post([FromBody] string value)
+        {
+        }
+        // PUT api/funcionario/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        // DELETE api/funcionario/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ApiSegunda/Models/Funcionario.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiSegunda.Models
+{
+    public class Funcionario
+    {
+        private string cidade { get; set; }
+        private int codigo { get; set; }
+        private string nome { get; set; }
+        private string setor { get; set; }
+        private string sobrenome { get; set; }
+    }
+}
